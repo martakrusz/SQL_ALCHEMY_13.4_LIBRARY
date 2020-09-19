96b1e051061dc724d0865dac4d3367ca384cebd2
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2020-09-19 22:19:02 +0200

    typo fixed (SECRED)

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRED_KEY = os.urandom(24)
+    SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('DATABASE_URL') or
            'sqlite:///' + os.path.join(BASE_DIR, 'library_data.db')
